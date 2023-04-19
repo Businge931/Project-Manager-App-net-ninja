8685fd590785e73d23af8add6435263958b8842e
Businge931 <jimmybusinge9@gmail.com>
2023-04-19 21:01:24 +0300

    initialize firebase and add context/custom hooks

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyktN0j-q-eMBpZ0-Hc7UfqQ8gorcYJ4c",
+  authDomain: "project-manager-net-ninja.firebaseapp.com",
+  projectId: "project-manager-net-ninja",
+  storageBucket: "project-manager-net-ninja.appspot.com",
+  messagingSenderId: "668756618199",
+  appId: "1:668756618199:web:a119b33c43f6f66c853992",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectAuth, projectFirestore, timestamp };
