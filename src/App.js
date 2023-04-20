f75895b9e66a8b46d3c160b62c59bd3a24e471c9
Businge931 <jimmybusinge9@gmail.com>
2023-04-20 22:48:30 +0300

    feat: set up protected routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -7,21 +7,45 @@ import Create from "./pages/create/Create";
 import ProjectDetails from "./pages/project/ProjectDetails";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { useAuthContext } from "./hooks/useAuthContext";
+import OnlineUsers from "./components/OnlineUsers";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <Sidebar />
-      <div className="container">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/projectdetails" element={<ProjectDetails />} />
-        </Routes>
-      </div>
+      {authIsReady && (
+        <>
+          {user && <Sidebar />}
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/signup"
+                element={!user ? <Signup /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/projects/:id"
+                element={user ? <ProjectDetails /> : <Navigate to="/login" />}
+              />
+            </Routes>
+          </div>
+          {user && <OnlineUsers />}
+        </>
+      )}
     </div>
   );
 }
