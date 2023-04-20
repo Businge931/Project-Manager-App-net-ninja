aa4699ca23db73787b69c0f8d541d8464db2f6fa
Businge931 <jimmybusinge9@gmail.com>
2023-04-20 23:06:14 +0300

    feat: created project form

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,8 +1,137 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+import { useNavigate } from "react-router-dom";
 import "./Create.css";
 
-const Create = () => {
-  return <div>Create</div>;
-};
+import { useCollection } from "../../hooks/useCollection";
+import { timestamp } from "../../firebase/config";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default Create;
+const categories = [
+  { value: "development", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+];
+
+export default function Create() {
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assigneUsers, setAssignedUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
+
+  const navigate = useNavigate();
+
+  //custom hooks
+  const { documents } = useCollection("users");
+  const { user } = useAuthContext();
+  const { addDocument, response } = useFirestore("projects");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setFormError(null);
+
+    if (!category) {
+      setFormError("Please select a project category");
+      return;
+    }
+    if (assigneUsers.length < 1) {
+      setFormError("Please assign the project to atleast 1 user");
+      return;
+    }
+
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid,
+    };
+    const assignedUsersList = assigneUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id,
+      };
+    });
+
+    const project = {
+      name,
+      details,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy,
+      assignedUsersList,
+    };
+
+    await addDocument(project);
+    // console.log(project);
+    if (!response.error) {
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map((user) => {
+        return { value: user, label: user.displayName };
+      });
+      setUsers(options);
+    }
+  }, [documents]);
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            type="text"
+            required
+            value={details}
+            onChange={(e) => setDetails(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            type="date"
+            required
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          <Select
+            options={categories}
+            onChange={(option) => setCategory(option)}
+          />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          <Select
+            options={users}
+            onChange={(option) => setAssignedUsers(option)}
+            isMulti
+          />
+        </label>
+        <button className="btn">Add Project</button>
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  );
+}
