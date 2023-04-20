495b4260fef6788845b320f1c71a2f32a926c7c7
Businge931 <jimmybusinge9@gmail.com>
2023-04-20 22:58:56 +0300

    feat:add pending and error states

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 import Temple from "../assets/temple.svg";
-import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <div className="navbar">
       <ul>
@@ -12,15 +17,30 @@ const Navbar = () => {
           <img src={Temple} alt="dojo log" />
           <span>The Dojo</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn">Logout</button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className="btn" disabled>
+                Logging out...
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </div>
   );
