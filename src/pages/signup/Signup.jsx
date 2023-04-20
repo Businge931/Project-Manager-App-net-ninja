ee4da284e06a795c84a3ca0e2c7c9b37cb957af8
Businge931 <jimmybusinge9@gmail.com>
2023-04-20 23:09:22 +0300

    feta:added signup form and authentication logic

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useSignup } from "../../hooks/useSignup";
 
 import "./Signup.css";
 
@@ -7,9 +8,40 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const { error, signup, isPending } = useSignup();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // signup(email, password, displayName, thumbnail);
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+
+    let selected = e.target.files[0];
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbnailError("selected file must be an image");
+      return;
+    }
+    if (selected.size > 100000) {
+      setThumbnailError("Image file size must be less that 100kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign up</h2>
       <label>
         <span>email:</span>
@@ -23,7 +55,7 @@ const Signup = () => {
       <label>
         <span>password:</span>
         <input
-          type="text"
+          type="password"
           required
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -38,6 +70,18 @@ const Signup = () => {
           onChange={(e) => setDisplayName(e.target.value)}
         />
       </label>
+      <label>
+        <span>profile thumbnail:</span>
+        <input type="file" required onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
+      </label>
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
