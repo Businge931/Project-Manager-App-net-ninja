9828ad12e2265ef54f88888324a0592670a35e14
Businge931 <jimmybusinge9@gmail.com>
2023-04-20 23:08:56 +0300

    feat: project details

--- src/pages/project/ProjectDetails.jsx
+++ src/pages/project/ProjectDetails.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./ProjectDetails.css";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+import ProjectSummary from "./ProjectSummary";
+import ProjectComments from "./ProjectComments";
 
 const ProjectDetails = () => {
-  return <div>ProjectDetails</div>;
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <ProjectSummary project={document} />
+      <ProjectComments project={document} />
+    </div>
+  );
 };
 
 export default ProjectDetails;
